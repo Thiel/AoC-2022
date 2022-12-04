cfe973c544197c025b9267b48f372a51b696d3a6
Jean-Luc Bounthong <jeanluc.bounthong@gmail.com>
2022-12-03 23:16:14 -0800

    solve d4

---
+++ d4/app/Main.hs
@@ -0,0 +1,41 @@
+import Data.List.Split
+import Data.List
+import System.IO
+
+sample = [
+  "2-4,6-8",
+  "2-3,4-5",
+  "5-7,7-9",
+  "2-8,3-7",
+  "6-6,4-6",
+  "2-6,4-8"
+  ]
+
+type Section = (Int, Int)
+type Assignment = (Section, Section)
+
+parse :: [String] -> [Assignment]
+parse input = map (\(w:x:y:z:[]) -> ((w,x),(y,z)))
+              $ map (map read)
+              $ map (splitWhen (\x->x=='-' || x == ',')) input
+
+overlap_fully :: Assignment -> Bool
+overlap_fully ((x1,y1),(x2,y2)) | x1 >= x2 && y1 <= y2 = True
+                          | x1 <= x2 && y1 >= y2 = True
+                          | otherwise = False
+
+solve :: [String] -> Int
+solve input = sum $ map (fromEnum . overlap_fully) $ parse input
+
+apply :: ([String] -> Int) -> IO Int
+apply s = do
+  h <- openFile "input" ReadMode
+  c <- hGetContents h
+  return $ s (lines c)
+
+overlap :: Assignment -> Bool
+overlap ((x1,y1),(x2,y2)) | length (intersect [x1..y1] [x2..y2]) > 0 = True
+                          | otherwise = False
+
+solve2 :: [String] -> Int
+solve2 input = sum $ map (fromEnum . overlap) $ parse input
