08e384ccb317cd6f8d6e36925721b0394bf76207
Jean-Luc Bounthong <jeanluc.bounthong@gmail.com>
2022-12-05 22:19:09 -0800

    clean up duplication

--- d6/app/Main.hs
+++ d6/app/Main.hs
@@ -3,11 +3,16 @@ import Data.List
 sample :: String
 sample = "bvwbjplbgvbhsrlpgdmjqwftvncz"
 
+search_uniq_seq :: Int -> String -> Maybe Int
+search_uniq_seq size ss = (+size) <$> (elemIndex size
+                                    $ map (length . group . sort)
+                                    $ (transpose . take size . tails) ss)
+
 solve :: String -> Maybe Int
-solve input = (+4) <$> (elemIndex 4 $ map (length . group . sort) $ (transpose . take 4 . tails) input)
+solve = search_uniq_seq 4
 
 sample2 :: String
 sample2 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
 solve2 :: String -> Maybe Int
-solve2 input = (+14) <$> (elemIndex 14 $ map (length . group . sort) $ (transpose . take 14 . tails) input)
+solve2 = search_uniq_seq 14
