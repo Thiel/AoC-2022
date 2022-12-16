29e4a313129b7ec1e2891c82ecc43b8e786a37fa
Jean-Luc Bounthong <jeanluc.bounthong@gmail.com>
2022-12-16 15:59:03 -0800

    solve d13 part 1

---
+++ d13/app/Main.hs
@@ -0,0 +1,87 @@
+import Text.Parsec
+import Text.Parsec.Number
+import System.IO
+import Debug.Trace
+
+sample = unlines [
+  "[1,1,3,1,1]",
+  "[1,1,5,1,1]",
+  "",
+  "[[1],[2,3,4]]",
+  "[[1],4]",
+  "",
+  "[9]",
+  "[[8,7,6]]",
+  "",
+  "[[4,4],4,4]",
+  "[[4,4],4,4,4]",
+  "",
+  "[7,7,7,7]",
+  "[7,7,7]",
+  "",
+  "[]",
+  "[3]",
+  "",
+  "[[[]]]",
+  "[[]]",
+  "",
+  "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+  "[1,[2,[3,[4,[5,6,0]]]],8,9]"]
+
+data Packet = Integer Int | List [Packet]
+  deriving (Eq)
+
+parse_entry :: Parsec String st Packet
+parse_entry = do
+  x <- parse_list
+       <|> parse_int
+  return x
+
+parse_list :: Parsec String st Packet
+parse_list = between (string "[") (string "]") parse_list_elt
+
+parse_list_elt :: Parsec String st Packet
+parse_list_elt = do
+  entries <- parse_entry `sepBy` (string ",")
+  return $ List entries
+
+parse_int :: Parsec String st Packet
+parse_int = do
+  i <- int
+  return $ Integer i
+
+parse_problem :: Parsec String st [(Packet, Packet)]
+parse_problem = (do left <- parse_entry
+                    newline
+                    right <- parse_entry
+                    spaces
+                    rest <- parse_problem
+                    return $ (left, right) : rest)
+                <|> return []
+
+solve input = sum [ idx | (idx, ord) <- zip [1..] pair_ord, ord]
+  where Right i = parse parse_problem "" input
+        pair_ord = map (\(a,b) -> a <= b) i
+
+instance Ord Packet where
+  -- (<=)
+  (Integer a) `compare` (Integer b) = a `compare` b
+  as@(List _) `compare` b@(Integer _) = as `compare` (List [b])
+  a@(Integer _) `compare` bs@(List _) = List [a] `compare` bs
+  (List as) `compare` (List bs) = as `compare` bs
+
+instance Show Packet where
+  show (Integer a) = show a
+  show (List as) = show as
+
+input :: IO String
+input = do
+  h <- openFile "input" ReadMode
+  hGetContents h
+
+debug input = unlines $ map custom_show $ zip i pair_ord
+  where Right i = parse parse_problem "" input
+        pair_ord = map (\(a,b) -> a <= b) i
+
+custom_show ((l,r), i) = show l ++ "\n" ++ show r ++ "\n" ++ show i ++ "\n"
+
